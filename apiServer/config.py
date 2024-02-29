efb5283af1066273edf022e3c6294017aa43712a
mynamecheff <albertdepalo@gmail.com>
2024-02-29 12:20:55 +0100

    init

--- apiServer/config.py
+++ apiServer/config.py
@@ -1,7 +1,8 @@
 import os
 
 # Tailscale API URL to get Devices
-TAILSCALE_API_URL = os.environ.get("TAILSCALE_API_URL", "https://api.tailscale.com/api/v2/tailnet/cyberpartners.dk/devices")
+TAILSCALE_API_URL = os.environ.get("TAILSCALE_API_URL", "https://api.tailscale.com/api/v2/tailnet/<org>/devices")
 
 # Tailscale API Key
-TAILSCALE_API_KEY = os.environ.get("TAILSCALE_API_KEY", "Bearer tskey-api-k2WDqr5CNTRL-5hS1AisPfDLnsgLsoTTQELEhTtZGfAvg")
+TAILSCALE_API_KEY = os.environ.get("TAILSCALE_API_KEY", "Bearer <key>")
+
